97365346524f90628e4ca0ab41ad4067df0fa7e6
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2023-11-17 21:45:10 +0100

    Update es2.py

--- 31-01-17/es2.py
+++ 31-01-17/es2.py
@@ -5,16 +5,26 @@
 
 
 def verifica(L,k):
+    # inizializzo contatore a 1
     c = 1
+    # seleziona il primo numero come ultimo elemento
     last_x = L[0]
+    # itera nella coda della lista (cioe' tutta la lista escluso il primo elemento)
     for i in range(1, len(L)):
+        # l'elemento attuale
         x = L[i]
+        # se l'ultimo elemento e' uguale all'attuale
         if last_x == x:
+            # incrementa il contatore
             c += 1
             if c == k:
                 return True
+        # vuol dire che il numero attuale e l'ultimo non sono uguali
         else:
+            # resetto il contatore a 1, questo perche' dire che il numero attuale
+            # e' uguale al precedente, significa che ci sono gia' 2 match
             c = 1
+        # il numero attuale sara' l'ultimo della prossima iterazione
         last_x = x
     return False
 
