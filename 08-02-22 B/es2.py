5d45cc006c243029083f7853dfd87b3defe3100f
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2023-11-20 23:52:37 +0100

    Update es2.py

--- 08-02-22 B/es2.py
+++ 08-02-22 B/es2.py
@@ -16,28 +16,27 @@ def verifica_lista(L,k):
         stringa_campione = L[i]
         # tutte le altre stringhe che vado a testare
         stringhe_target = L[i-k:i]
-        print(stringa_campione, stringhe_target)
         # itero per ogni stringa target
         # per ogni stringa target, esiste almeno una lettera in comune con una lettera della stringa campione?
         for stringa_target in stringhe_target:
-            # inizializzo ad ogni nuova stringa target
+            # riaggiorno ad ogni nuova stringa target
             esiste_lettera = False
             # inizializzo, lo uso per iterare su ogni lettera della stringa campione
             j = 0
             # itero per ogni lettera della stringa campione
             while not esiste_lettera and j<len(stringa_campione):
-                # ogni lettera della stringa attuale
+                # ogni lettera della stringa campione
                 l = stringa_campione[j]
                 # se esiste almeno una lettera nella stringa target, questo mi basta,
                 # puoi uscire dal while loop e passare alla prossima stringa target
                 if l in stringa_target:
-                    # esco dal loop perche' ho trovato almeno una lettera in comune in ogni altra stringa
+                    # esco dal loop perche' ho trovato almeno una lettera in comune in questa stringa target,
+                    # quindi passa alla prossima stringa target
                     esiste_lettera = True
                 j += 1
-
             # solo dopo il while loop, cioe' dopo che ho testato ogni lettera della stringa campione contro una stringa target
             # (una alla volta), posso dire per certo se c'era almeno una lettera in comune
-            # se non c'era nessuna lettera in comune, allora esci
+            # se non c'era nessuna lettera in comune, allora ritorna falso
             if not esiste_lettera:
                 return False
     # condizione richiesta dalla traccia soddisfatta
